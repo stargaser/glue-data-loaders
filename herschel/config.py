5fc81ae017c9f7217357b8b6eadf3af85bb45272
David Shupe <shupe@morpheus.ipac.caltech.edu>
2015-02-03 16:52:36 -0800

    Rewrite to handle wavelengths for cubes

--- herschel/config.py
+++ herschel/config.py
@@ -5,37 +5,45 @@ from glue.core.data_factories import has_extension
 
 import astropy.io.fits as fits
 from astropy.wcs import WCS
+import numpy as np
 
 
 @data_factory('Herschel Image', has_extension('fits fit fits.gz'), default='fits fit fits.gz')
 def herschel_data(filename):
     """
-    Data loader customized for 'typical' hubble fits files
+    Data loader customized for Herschel fits files
 
-    This function extracts extensions named 'image'
-    from a file. Each is retuned as a glue Data object
+    This function extracts extension named 'image',
+    'error', 'coverage', etc
+    from a file. Each is retuned as a glue Data object.
+    To handle PACS cubes, if ImageIndex extension is present
+    it is used to provide wavelengths
 
     astropy.wcs.WCS objects are used to parse wcs.
 
     Any other extensions are ignored
     """
 
-    result = []
 
     hdulist = fits.open(filename, memmap=True)
 
-    index = 1
+    d = Data("data")
+    # Fix for invalid CUNIT values in 12.1 PACS data
+    for c in ['CUNIT1', 'CUNIT2']:
+        if hdulist['image'].header[c]:
+            del hdulist['image'].header[c]
+    d.coords = coordinates_from_wcs(WCS(hdulist['image'].header))
+    wavelengths = None
+    for h in hdulist:
+        if (h.name in ['image', 'error', 'cov']):
+            d.add_component(hdulist[h.name].data, name)
+        if (h.name == 'ImageIndex'):
+            wavelengths = hdulist[h.name].data.field(0)
+            
+    # Fix up wavelengths if needed
+    if (wavelengths and d['Wavelength'].shape[0] == len(wavelengths)):
+        warray = np.zeros(d['Wavelength'].shape, dtype=d['Wavelength'].dtype)
+        warray += wavelengths[:, np.newaxis, np.newaxis]
+        d['Wavelength'] = warray
 
-    def _get_image_ext(i, index):
-        d = Data("image_%i" % (index))
-        d.coords = coordinates_from_wcs(WCS(hdulist[i].header))
-        d.add_component(hdulist[i].data, 'PRIMARY')
-        return d
-
-    for i, h in enumerate(hdulist):
-        if h.name != 'image':
-            continue
-        result.append(_get_image_ext(i, index))
-        index += 1
-
-    return result
+    return d
